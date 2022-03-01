55cf33046a93a8d975270e88ac9679ff4e13cdaa
Swathy <swatses2001@gmail.com>
2022-03-01 15:10:46 +0530

    made changes

--- app/controllers/admin/get-books.js
+++ app/controllers/admin/get-books.js
@@ -5,29 +5,23 @@ import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import MutableArray from '@ember/array/mutable';
 import { get } from '@ember/object';
+import { filterBy } from '@ember/object/computed';
 
 export default class AdminGetBooksController extends Controller {
   @service store;
   @tracked selectedBooks = [];
+  @tracked books=this.model;
 
-  @action
-  getSelectedBooks(book) {
-    if (!book.isChecked) {
-      this.selectedBooks.addObject(book.id);
-    } else {
-      this.selectedBooks.removeObject(book.id);
-    }
-  }
 
   @action
   deleteSelectedBooks() {
-    if (window.confirm('Are you sure about deleting this book?')) {
-     
-      this.selectedBooks.forEach(element => {
-        this.book = this.store.peekRecord('book', element);
+   if (window.confirm('Are you sure about deleting this book?')) {
+     this.books.forEach(element => {
+        if(element.isChecked){
+        this.book = this.store.peekRecord('book', element.id);
         this.book.destroyRecord();
+        }
       });
-      this.selectedBooks.clear();
       alert('Records deleted');
     }
   }
