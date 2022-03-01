5e9afda215f36e4091040322ce477568f1e98b29
Swathy <swatses2001@gmail.com>
2022-03-01 12:53:41 +0530

    small change

--- app/controllers/admin/get-book-by-id.js
+++ app/controllers/admin/get-book-by-id.js
@@ -4,13 +4,14 @@ import window from 'ember-window-mock';
 import { inject as service } from '@ember/service';
 
 export default class AdminGetBookByIdController extends Controller {
+  @service router;
   @service store;
   @action
   deleteBook(model) {
     if (window.confirm('Are you sure about deleting this book?')) {
       this.book = this.store.peekRecord('book', model.id);
       this.book.destroyRecord();
-      this.transitionToRoute('admin.getBooks');
+      this.router.transitionTo('admin.getBooks');
     }
   }
 }
